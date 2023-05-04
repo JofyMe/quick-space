4495bc47d963b441e538004a507f5d149f6c403d
JofyMe <mr.tretyak15usa@gmail.com>
2023-05-04 05:37:06 +0300

    [V004] Add Brands block + create slider for adaptation

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,31 @@ document.querySelector('.header__burger').addEventListener('click', function() {
     this.classList.toggle('active');
     document.querySelector('.header__navbar').classList.toggle('open')
 })
+
+    var swiper = new Swiper(".mySwiper", {
+      slidesPerView: 1,
+      spaceBetween: 10,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      breakpoints: {
+        320: {
+          slidesPerView: 2,
+          spaceBetween: 0,
+        },
+        480: {
+          slidesPerView: 3,
+          spaceBetween: 0,
+        },
+        992: {
+          slidesPerView: 5,
+          spaceBetween: 0,
+        },
+        1200: {
+          slidesPerView: 5,
+          spaceBetween: 0,
+        },
+      },
+    });
+
